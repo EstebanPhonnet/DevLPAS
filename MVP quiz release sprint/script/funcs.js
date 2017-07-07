588255ea918066222c3f65c39e9db19e92ce091d
EstebanPhonnet <esteban.phonnet@gmail.com>
2017-07-07 11:55:23 +0200

    fix bug
    todo test for demo

--- MVP quiz release sprint/script/funcs.js
+++ MVP quiz release sprint/script/funcs.js
@@ -1,5 +1,7 @@
  function unblur() {
   onFocus();
+  $("#questionLabel").css('visibility', 'visible');
+  $("#tipsContainer").css('visibility', 'visible');
   $("#quiz").unblock();
  }
 
@@ -17,6 +19,8 @@ function autoPlayVideo(){
   $("#quiz").block({ message: null });
   // $("#videoContainer").unblock();
   onBlur();
+  $("#questionLabel").css('visibility', 'hidden');
+  $("#tipsContainer").css('visibility', 'hidden');
   $("#videoContainer").html('<div id="iframe-container"><h3>Vous avez une petite erreur, voici une vidéo explicative pour vous aider</h3><a href="#" class="closeBtn" onclick="this.parentNode.parentNode.removeChild(this.parentNode);unblur();"> >>>Fermer la vidéo<<< </a>' +
     '<iframe id="tipsVideo" width="840" height="473" src="https://www.youtube.com/embed/TfLmXUrFFPw?autoplay=1" frameborder="0" allowfullscreen wmode="opaque"></iframe></div>');
 }
